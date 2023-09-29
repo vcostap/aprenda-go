197096520d209ec0614f972e6219fdb4b448e833
Vinícius Costa <viniciuscostapereira@hotmail.com>
2023-09-29 20:06:13 -0300

    nivel_11 -> exercicio_1 e 2

---
+++ nivel_11/exercicio_02.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type person struct {
+	First   string
+	Last    string
+	Sayings []string
+}
+
+func main() {
+	p1 := person{
+		First:   "James",
+		Last:    "Bond",
+		Sayings: []string{"Shaken, not stirred", "Any last wishes?", "Never say never"},
+	}
+
+	bs, err := toJSON(p1)
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println(string(bs))
+}
+
+func toJSON(a interface{}) ([]byte, error) {
+	bs, err := json.Marshal(a)
+	if err != nil {
+		return nil, fmt.Errorf("Errou no Marshal: %v", err)
+	}
+
+	return bs, nil
+}
